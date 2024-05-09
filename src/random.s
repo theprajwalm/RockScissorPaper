ba84ead7ca6d02171a5767f7db03153f38fc1e9a
theprajwalm <prmi00003@stud.uni-saarland.de>
2024-05-09 22:41:24 +0200

    firstCommit

--- src/random.s
+++ src/random.s
@@ -1,5 +1,7 @@
 # vim:sw=2 syntax=asm
 .data
+seed:
+
 
 .text
   .globl gen_byte, gen_bit
@@ -18,7 +20,25 @@
 #  If 11 would be returned, produce two new bits until valid
 #
 gen_byte:
-  # TODO
+#saving the $ra of the gen_byte
+  add $sp $sp -4  #allocating 4 byte
+  sw  $ra 0($sp) #storing $ra
+  li $t0 2 #loads the value 2 to compare
+  
+  gen_two_num:
+  jal gen_bit #calling gen_bit for the first value
+  move $t1 $v0 #moves the value of $v0 t0 $t1
+  
+  jal gen_bit #calling gen_bit for the second value
+  move $t2 $v0 #moves the value of $v0 to $t2
+
+  addu $t3 $t1 $t2 #adding the value of $t1 and $t2
+  beq $t0 $t3 gen_two_num #comparing the value with 2
+  sll $t1 $t1 1 #shifting the value to right by 1
+  or $v0 $t1 $t2 #concatenating two registers and storing in $vo
+  
+  lw $ra 0($sp) #restoring the value of the $ra
+  add $sp $sp 4 #restoring the stack pointer back to the intial value
   jr $ra
 
 # Arguments:
@@ -35,5 +55,8 @@ gen_byte:
 #  Put the computed bit into $v0
 #
 gen_bit:
-  # TODO
+  li $v0 41 #generate random number
+  li $a0 0
+  syscall
+  andi $v0 $a0 1 #stores the least bit value
   jr $ra
--- src/rps.s
+++ src/rps.s
@@ -1,5 +1,9 @@
 # vim:sw=2 syntax=asm
+
 .data
+tie_msg:.asciiz "T"
+lose_msg:.asciiz "L"
+win_msg:.asciiz "W"
 
 .text
   .globl play_game_once
@@ -19,5 +23,54 @@
 #
 # Returns: Nothing, only print either character 'W', 'L', or 'T' to stdout
 play_game_once:
-  # TODO
+  addiu $sp $sp -4 #storing $ra
+  sw $ra 0($sp)
+  
+  jal gen_byte #calling gen_byte for the first player
+  move $s0 $v0 
+  jal gen_byte #calling gen_byte for the second player
+  move $s1 $v0
+  
+  lw $ra 0($sp)
+  addiu $sp $sp 4
+  
+  beq $s1 $s0 tie #comparing the two players
+  
+  li $t0 0 #rock
+  beq $s0 $t0 rock #player 1 is rock
+  li $t0 1 #paper
+  beq $s0 $t0 paper #player 1 is paper
+  j scissor #player 1 is scissor
+  
+  rock:
+  li $t0 2 #Scissors
+  beq $s1 $t0 win  
+  j lose
+  
+  paper:
+  li $t0 0 #rock
+  beq $s1 $t0 win
+  j lose
+  
+  scissor:
+  li $t0 1 #paper
+  beq $s1 $t0 win
+  j lose
+  
+  win:
+  li $v0 4
+  la $a0 win_msg
+  syscall 
   jr $ra
+  
+  lose:
+  li $v0 4
+  la $a0 lose_msg
+  syscall
+  jr $ra 
+  
+  tie:
+  li $v0 4
+  la $a0 tie_msg
+  syscall 
+  jr $ra 
