96d430b4009fc9b565214215468fe156d6aa6a41
theprajwalm <prmi00003@stud.uni-saarland.de>
2024-05-10 23:43:52 +0200

    printTapePrintAllButWrong

--- src/automaton.s
+++ src/automaton.s
@@ -1,4 +1,9 @@
 # vim:sw=2 syntax=asm
+.data
+alive: .asciiz "X"
+dead: .asciiz "_"
+end: .asciiz "\n"
+
 .text
   .globl simulate_automaton, print_tape
 
@@ -34,5 +39,41 @@ simulate_automaton:
 #   Print:  
 #       __X_X_X_
 print_tape:
-  # TODO
+  addiu $sp $sp -4 #saving $ra
+  sw $ra 0($sp)
+  
+  lw $t0 4($a0) #loading  tape
+  lb $t2 8($a0) #tape_len
+  li $t3 0 #loopCounter
+  
+  loop:
+  andi $t1 $t0 1 #getting the lsb
+  srl $t0 $t0 1 #1 shift right
+  
+  bne $t1 $zero printAlive #conditions
+  b printDead
+  
+  printAlive:
+  la $a0 alive
+  li $v0 4
+  syscall
+  j continue
+  
+  printDead:
+  la $a0 dead
+  li $v0 4
+  syscall
+  																																																		
+  continue:
+  addiu $t3 $t3 1 #increasing the loop counter
+  bne $t3 $t2 loop #checking if we are done or not with all bits
+  
+  programEnd:
+  la $a0 end
+  li $v0 4
+  syscall
+  
+  lw $ra 0($sp) #restoring $ra
+  addiu $sp $sp 4 
+  
   jr $ra
