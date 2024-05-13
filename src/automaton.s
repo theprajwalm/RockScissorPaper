4318863ef04b6f14d85d6a82b2fadab2883f597d
theprajwalm <prmi00003@stud.uni-saarland.de>
2024-05-13 19:19:32 +0200

    assignment 6 fixed

--- src/automaton.s
+++ src/automaton.s
@@ -18,8 +18,64 @@ over: .asciiz "\n"
 #  11($a0): column    (1 byte)
 #
 # Returns: Nothing, but updates the tape in memory location 4($a0)
+
 simulate_automaton:
-  # TODO
+  addi $sp $sp -12        	#allocate space on stack
+  sw $ra 0($sp)            	#store $ra
+  sw $s0 4($sp)            	#store $s0
+  sw $s1 8($sp)           	#store $s1
+  
+  lw $t0 4($a0)            	#load tape
+  lb $t1 8($a0)            	#load tape length
+  lb $t2 9($a0)            	#load rule
+  li $t3 0                 	#Counter loop
+  li $t4 0                 	#place for new tape
+
+tloop:
+  add $s0 $t3 -1          	#index of right neighbor 
+  add $s1 $t3 1           	#index of left neighbor
+  blt $s0 0 left 	#go to left end if right index is negative
+  bge $s1 $t1 right  	#go to right end if left index is length or greater
+  j nachbar   	#else go to neighbor calculation 
+
+left:
+  add $s0, $s0, $t1         	#go to left end by adding the length
+  j nachbar  
+
+right:
+  sub $s1, $s1, $t1        	 #go to right end by subtracting the length
+
+nachbar:
+  srlv $t5 $t0 $s0        	#right neighbor
+  andi $t5 $t5 1
+
+  srlv $t6 $t0 $t3        	#current cell
+  andi $t6 $t6 1
+
+  srlv $t7 $t0 $s1        
+  andi $t7 $t7 1	    	#left neighbor
+
+  sll $t7 $t7 2           	#left shift the left neighbor to create space
+  sll $t6 $t6 1			#left shift current cell as well
+  or $t7 $t7 $t6		#combine left and current cell
+  or $t7 $t7 $t5          	#combine right cell as well
+
+  srlv $t7 $t2 $t7        	#extract the corresponding bit from the rule
+  andi $t7 $t7 1
+
+  sllv $t7 $t7 $t3        
+  or $t4 $t4 $t7		#save the new state in tape 
+
+  addiu $t3 $t3 1
+  blt $t3 $t1 tloop       	#loop for all cells
+
+  sw $t4 4($a0)            	#store the new tape
+
+  lw $ra 0($sp)            	#load $ra
+  lw $s0 4($sp)            	#load $s0
+  lw $s1 8($sp)           	#load $s1
+  addi $sp $sp 12         	#deallocate space on stack
+  jr $ra
   jr $ra
 
 # Print the tape of the cellular automaton
