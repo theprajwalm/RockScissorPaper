d0ffde2780c4bb396827e4e75e5540eb0794f6f1
theprajwalm <prmi00003@stud.uni-saarland.de>
2024-05-14 11:08:32 +0200

    last commit

--- src/automaton.s
+++ src/automaton.s
@@ -20,7 +20,65 @@ over: .asciiz "\n"
 # Returns: Nothing, but updates the tape in memory location 4($a0)
 
 simulate_automaton:
+  subi $sp $sp 12  #allocate space on stack
+  sw $ra 0($sp)    #store $ra
+  sw $s1 4($sp)    #store $s1
+  sw $s0 8($sp)    #store $s0
+  
+  lw $t1 4($a0)  #load tape
+  lb $t0 8($a0)  #load tape length
+  lb $t2 9($a0)  #load rule
+  li $t3 0       #loop counter
+  li $t4 0       #place for new tape
+
+tloop:
+  add $s1 $t3 -1  #index of right neighbor 
+  add $s0 $t3 1  #index of left neighbor
+  blt $s1 0 lend  	#go to left end if right index is negative
+  bge $s0 $t0 rend #go to right end if left index is length or greater
+  j neighbor   	#else go to neighbor calculation 
+
+rend:
+  sub $s0, $s0, $t0 #go to right end by subtracting the length
+   j neighbor   #go to fetch 3 neighbors
+
+lend:
+  add $s1, $s1, $t0  #go to left end by adding the length
+ 
+neighbor:
+  srlv $t5 $t1 $s1  #right neighbor
+  andi $t5 $t5 1
+
+  srlv $t6 $t1 $t3 #current cell
+  andi $t6 $t6 1
+
+  srlv $t7 $t1 $s0        
+  andi $t7 $t7 1	#left neighbor
+
+adding:
+  sll $t7 $t7 2  #left shift the left neighbor
+  sll $t6 $t6 1	#left shift current cell as well
+  or $t7 $t7 $t6  #combine left and current cell
+  or $t7 $t7 $t5  #combine right cell as well
+
+  srlv $t7 $t2 $t7  #extract the corresponding bit from the rule
+  andi $t7 $t7 1
+
+  sllv $t7 $t7 $t3        
+  or $t4 $t4 $t7	#save the new state in tape 
+
+  addiu $t3 $t3 1
+  blt $t3 $t0 tloop  #loop for all cells
+
+store:
+  sw $t4 4($a0) #store the new tape
+
+  lw $ra 0($sp)  #load $ra
+  lw $s1 4($sp)   #load $s0
+  lw $s0 8($sp)   #load $s1
+  subi $sp $sp -12  #deallocate space on stack
   jr $ra
+
 
 # Print the tape of the cellular automaton
 # Arguments:
@@ -45,8 +103,8 @@ print_tape:
   
   li $t2 0 #loop counter
   
-  loop:
-  beq $t1 $t2 end #if tape_len and loop number equal
+  loop1:
+  beq $t1 $t2 end1 #if tape_len and loop number equal
   subi $t5 $t1 1 #estimate the length to be shifted
   sub $t6 $t5 $t2 #for correct shift number
   srlv $t7 $t0 $t6 #shifting
@@ -68,9 +126,9 @@ print_tape:
   
   continue:
   addiu $t2 $t2 1 #increase the loop counter
-  j loop
+  j loop1
   
-  end: #print \n
+  end1: #print \n
   la $a0 over
   li $v0 4
   syscall
